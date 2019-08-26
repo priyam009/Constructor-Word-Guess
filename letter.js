e767065337dad026fc03a4262f4716081a8557e3
Priyam <priyam009@live.com>
2019-08-26 23:23:17 +1000

    Create letter constructor to check single character value with the correct answer

---
+++ letter.js
@@ -0,0 +1,23 @@
+//Letter Constructor to take single character value
+var Letter = function(value) {
+this.value = value;
+this.isGuessed = false;
+};
+
+//Constructor method to print the correct value or "_"
+Letter.prototype.toString = function() {
+if (!this.isGuessed) {
+return "_";
+} else {
+return this.value;
+}
+};
+
+//Constructor methos to check the user input with the correct answer
+Letter.prototype.guess = function(letter) {
+if (letter === this.value) {
+this.isGuessed = true;
+}
+};
+
+module.exports = Letter;
